beee1c1d0fba438e1a624df052367af38c522407
Alexander Wilden <october.rust@gmx.de>
2012-05-24 20:33:25 +0200

    repaying now works for single vouchers.

--- web/js/controllers/ListVouchersController.js
+++ web/js/controllers/ListVouchersController.js
@@ -1,15 +1,15 @@
 function ListVouchersController( $scope ) {
    'use strict';
-   
+
    $scope.$emit( 'toggleLoadingIndicator', true, "Lade Kassenzettel ..." );
-   
+
    $scope.vouchers = [];
    $scope.debts = [];
-   
+
    function refresh() {
       now.getNonRepaidVouchers( function( err, vouchers ) {
          $scope.vouchers = vouchers;
-         
+
          now.calculateDebts( vouchers, function( err, debts ) {
             $scope.debts = debts;
             $scope.$emit( 'toggleLoadingIndicator', false );
@@ -18,36 +18,43 @@ function ListVouchersController( $scope ) {
       } );
    }
    refresh();
-   
+
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-   
+
    $scope.refresh = refresh;
-   
+
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-   
+
    $scope.markAllRepaid = function() {
       console.log( "all ..." );
    };
-   
+
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-   
+
    $scope.markRepaid = function( voucher ) {
-      console.log( "repaid %o", voucher );
-      now.setVoucherToRepaid(voucher,function(repaidVoucher){
-         console.log( repaidVoucher );
-      })
+      now.setVoucherToRepaid( voucher, function( err, repaidVoucher ) {
+         
+      } );
    };
-   
+
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-   
+
    $scope.deleteVoucher = function( voucher ) {
       console.log( "delete %o", voucher );
    };
-   
+
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-   
+
    $scope.editVoucher = function( voucher ) {
       console.log( "edit %o", voucher );
    };
    
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+   
+   $scope.$on( 'serverEvent', function( event, serverEvent ) {
+      if( serverEvent.type === 'voucherRepaid' ) {
+         refresh();
+      }
+   } );
+
 }
